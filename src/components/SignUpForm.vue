9b30f784f3afc78123a4d2a7b14244e7636a0466
matiunivar <mattff3@gmail.com>
2022-02-04 16:53:14 -0300

    fixign validation

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -91,7 +91,7 @@
                 this.passwordError = this.password.length > 5 ? 
                     '' : 'Password must be at least 6 characters long';
                 
-                if(this.passwordError) {
+                if(!this.passwordError) {
                     console.log('email:' + this.email);
                     console.log('password:' + this.password);
                     console.log('role:' + this.role);
